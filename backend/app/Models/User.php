8ab5acdf095a7ca8d9f3eee08311252701770437
ychihiro <y.chihiro0510@gmail.com>
2025-04-29 22:06:18 +0900

    fix: 姓名でカラムを分ける

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -13,7 +13,8 @@ class User extends Authenticatable
   use HasApiTokens, HasFactory, Notifiable;
 
   protected $fillable = [
-    'name',
+    'first_name',
+    'last_name',
     'email',
     'phone_number',
     'password',
